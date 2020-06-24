b393ea19c0c50ed67e70e192935199c7f91f136e
mikepiven <mihail.piven@gmail.com>
2020-06-24 16:01:48 +0300

    Занятие 2. Функция sumSalary(salaries)

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,5 +4,18 @@
  * @returns {Number}
  */
 function sumSalary(salaries) {
-  // ваш код...
+  let result = 0
+  for (const key in salaries) {
+    result += typeof salaries[key] === 'number' ? salaries[key] : 0
+  }
+  return result
 }
+
+// let salaries = {
+//   John: 1000,
+//   Ann: 1600,
+//   Pete: 1300,
+//   month: 'December',
+//   currency: 'USD',
+//   isPayed: false
+// }
