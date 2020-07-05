99d8982e93b8e4f4edf30c9d7282d306f99cdb4e
mikepiven <mihail.piven@gmail.com>
2020-07-05 21:49:19 +0300

    Занятие 3. Найти min/max в строке

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -4,5 +4,16 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
-  // ваш код...
+  let result = {
+    min: 0,
+    max: 0
+  }
+  const tmp1 = str.split(' ').filter(i => !isNaN(i) && Boolean(i))
+  const tmp2 = str.split(' ').filter(i => isNaN(i)).join().split(',').filter(i => !isNaN(i) && Boolean(i))
+  const numbers = tmp1.concat(tmp2).map(i => +i).sort((a, b) => a - b)
+  if (numbers.length >= 2) {
+    result.min = numbers[0]
+    result.max = numbers[numbers.length - 1]
+  }
+  return result
 }
