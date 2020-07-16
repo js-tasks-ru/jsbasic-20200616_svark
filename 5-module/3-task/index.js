563f0839b22a05b39483e099759955ded1db9a3a
mikepiven <mihail.piven@gmail.com>
2020-07-16 22:16:29 +0300

    Занятие 5. Учебный проект (пролог): Карусель

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,23 @@
-function initCarousel() {
-  // ваш код...
+const arrows = {
+  left: document.querySelector('.carousel__arrow_left'),
+  right: document.querySelector('.carousel__arrow_right'),
 }
+const inner = document.querySelector('.carousel__inner')
+let currentSlide = 0
+
+function initCarousel() {
+  arrows.left.style.display = 'none'
+  arrows.left.addEventListener('click', switchCarousel)
+  arrows.right.addEventListener('click', switchCarousel)
+}
+
+function switchCarousel(event) {
+  event.target.src.indexOf('left') !== -1
+    ? currentSlide--
+    : currentSlide++
+  inner.style.transform = `translateX(-${inner.offsetWidth * currentSlide}px)`
+  arrows.left.style.display = currentSlide === 0 ? 'none' : ''
+  arrows.right.style.display = currentSlide === 3 ? 'none' : ''
+}
+
+initCarousel()
