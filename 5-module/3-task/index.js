ba92b2993eb2b9bbeeb36c4eab4571428cf18c23
mikepiven <mihail.piven@gmail.com>
2020-07-17 21:42:53 +0300

    Занятие 5. Учебный проект (пролог): Карусель (исправлен для прохождения автотестов)

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,23 +1,22 @@
-const arrows = {
-  left: document.querySelector('.carousel__arrow_left'),
-  right: document.querySelector('.carousel__arrow_right'),
-}
-const inner = document.querySelector('.carousel__inner')
-let currentSlide = 0
 
 function initCarousel() {
+  const arrows = {
+    left: document.querySelector('.carousel__arrow_left'),
+    right: document.querySelector('.carousel__arrow_right'),
+  }
+  const inner = document.querySelector('.carousel__inner')
+  let currentSlide = 0
   arrows.left.style.display = 'none'
-  arrows.left.addEventListener('click', switchCarousel)
-  arrows.right.addEventListener('click', switchCarousel)
+  arrows.left.addEventListener('click', () => {
+    currentSlide--
+    inner.style.transform = `translateX(-${inner.offsetWidth * currentSlide}px)`
+    arrows.left.style.display = currentSlide === 0 ? 'none' : ''
+    arrows.right.style.display = currentSlide === 3 ? 'none' : ''
+  })
+  arrows.right.addEventListener('click', () => {
+    currentSlide++
+    inner.style.transform = `translateX(-${inner.offsetWidth * currentSlide}px)`
+    arrows.left.style.display = currentSlide === 0 ? 'none' : ''
+    arrows.right.style.display = currentSlide === 3 ? 'none' : ''
+  })
 }
-
-function switchCarousel(event) {
-  event.target.src.indexOf('left') !== -1
-    ? currentSlide--
-    : currentSlide++
-  inner.style.transform = `translateX(-${inner.offsetWidth * currentSlide}px)`
-  arrows.left.style.display = currentSlide === 0 ? 'none' : ''
-  arrows.right.style.display = currentSlide === 3 ? 'none' : ''
-}
-
-initCarousel()
